e9fcf2b975e9ce77caba365aa0f1f5642e9b867a
Rayen-Toujani <toujani.mohamedrayen@esprit.tn>
2023-11-28 16:26:19 +0100

    prosit 11

--- src/models/Managment.java
+++ src/models/Managment.java
@@ -14,8 +14,8 @@ public interface Managment<Student> {
 
     public   void displayStudents(List<Student> students,
                          Consumer<Student> con);
-    //public  void displayStudentsByFilter(List<Student> students, Predicate<Student> pre, Consumer<Student> con);
-    //public     String returnStudentsNames(List<Student> students, Function<Student, String> fun);
+    public  void displayStudentsByFilter(List<Student> students, Predicate<Student> pre, Consumer<Student> con);
+    public     String returnStudentsNames(List<Student> students, Function<Student, String> fun);
     //public  Student createStudent(Supplier<Student> sup);
     //public     List<Student> sortStudentsById(List<Student> students, Comparator<Student> com);
     //public Stream<Student> convertToStream(List<Student> students);
--- src/models/StudentManagment.java
+++ src/models/StudentManagment.java
@@ -2,6 +2,8 @@ package models;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class StudentManagment implements Managment<Etudiant> {
 
@@ -14,6 +16,30 @@ public class StudentManagment implements Managment<Etudiant> {
     }
     }
 
+
+    public  void displayStudentsByFilter(List<Etudiant> etudiants, Predicate<Etudiant> pre, Consumer<Etudiant> con){
+
+        for (Etudiant etudiant : etudiants) {
+            if (pre.test(etudiant)) {
+                con.accept(etudiant);
+            }
+        }
+
+
+    }
+
+
+    public     String returnStudentsNames(List<Etudiant> etudiants, Function<Etudiant, String> fun){
+
+        StringBuilder noms = new StringBuilder();
+        for (Etudiant etudiant : etudiants) {
+            noms.append(fun.apply(etudiant)).append(", ");
+        }
+        return noms.toString();
+
+
+
+    }
 
 
 
